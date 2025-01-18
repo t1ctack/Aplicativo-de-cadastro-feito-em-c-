50cbcee6b1936625212938690edbb92167487847
Luis Felipe <louid090louiddrews@gmail.com>
2025-01-18 15:07:08 -0300

    Finalizando aba de modificacoes

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -42,8 +42,21 @@
             textBoxNomeRua = new TextBox();
             textBoxNumeroCasa = new TextBox();
             button1 = new Button();
+            label6 = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            label9 = new Label();
+            label10 = new Label();
+            labelNomeCompleto = new Label();
+            labelDataNascimento = new Label();
+            labelNomeRua = new Label();
+            labelNumeroCasa = new Label();
+            textBox1 = new TextBox();
+            buttonBusca = new Button();
+            buttonExcluir = new Button();
             tabControl1.SuspendLayout();
             tabPage1.SuspendLayout();
+            tabPage2.SuspendLayout();
             SuspendLayout();
             // 
             // tabControl1
@@ -79,6 +92,18 @@
             // 
             // tabPage2
             // 
+            tabPage2.Controls.Add(buttonExcluir);
+            tabPage2.Controls.Add(buttonBusca);
+            tabPage2.Controls.Add(textBox1);
+            tabPage2.Controls.Add(labelNumeroCasa);
+            tabPage2.Controls.Add(labelNomeRua);
+            tabPage2.Controls.Add(labelDataNascimento);
+            tabPage2.Controls.Add(labelNomeCompleto);
+            tabPage2.Controls.Add(label6);
+            tabPage2.Controls.Add(label7);
+            tabPage2.Controls.Add(label8);
+            tabPage2.Controls.Add(label9);
+            tabPage2.Controls.Add(label10);
             tabPage2.Location = new Point(4, 24);
             tabPage2.Name = "tabPage2";
             tabPage2.Padding = new Padding(3);
@@ -176,6 +201,112 @@
             button1.Text = "Cadastrar";
             button1.UseVisualStyleBackColor = true;
             // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(9, 147);
+            label6.Name = "label6";
+            label6.Size = new Size(96, 15);
+            label6.TabIndex = 9;
+            label6.Text = "Número da casa:";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(9, 121);
+            label7.Name = "label7";
+            label7.Size = new Size(79, 15);
+            label7.TabIndex = 8;
+            label7.Text = "Nome da rua:";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(9, 96);
+            label8.Name = "label8";
+            label8.Size = new Size(115, 15);
+            label8.TabIndex = 7;
+            label8.Text = "Data de nascimento:";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Location = new Point(6, 71);
+            label9.Name = "label9";
+            label9.Size = new Size(136, 15);
+            label9.TabIndex = 6;
+            label9.Text = "Número do documento:";
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Location = new Point(6, 45);
+            label10.Name = "label10";
+            label10.Size = new Size(97, 15);
+            label10.TabIndex = 5;
+            label10.Text = "Nome completo:";
+            // 
+            // labelNomeCompleto
+            // 
+            labelNomeCompleto.AutoSize = true;
+            labelNomeCompleto.Location = new Point(108, 45);
+            labelNomeCompleto.Name = "labelNomeCompleto";
+            labelNomeCompleto.Size = new Size(12, 15);
+            labelNomeCompleto.TabIndex = 10;
+            labelNomeCompleto.Text = "-";
+            // 
+            // labelDataNascimento
+            // 
+            labelDataNascimento.AutoSize = true;
+            labelDataNascimento.Location = new Point(130, 96);
+            labelDataNascimento.Name = "labelDataNascimento";
+            labelDataNascimento.Size = new Size(12, 15);
+            labelDataNascimento.TabIndex = 12;
+            labelDataNascimento.Text = "-";
+            // 
+            // labelNomeRua
+            // 
+            labelNomeRua.AutoSize = true;
+            labelNomeRua.Location = new Point(91, 121);
+            labelNomeRua.Name = "labelNomeRua";
+            labelNomeRua.Size = new Size(12, 15);
+            labelNomeRua.TabIndex = 13;
+            labelNomeRua.Text = "-";
+            // 
+            // labelNumeroCasa
+            // 
+            labelNumeroCasa.AutoSize = true;
+            labelNumeroCasa.Location = new Point(108, 147);
+            labelNumeroCasa.Name = "labelNumeroCasa";
+            labelNumeroCasa.Size = new Size(12, 15);
+            labelNumeroCasa.TabIndex = 14;
+            labelNumeroCasa.Text = "-";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(148, 63);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(209, 23);
+            textBox1.TabIndex = 15;
+            // 
+            // buttonBusca
+            // 
+            buttonBusca.Location = new Point(6, 186);
+            buttonBusca.Name = "buttonBusca";
+            buttonBusca.Size = new Size(97, 35);
+            buttonBusca.TabIndex = 16;
+            buttonBusca.Text = "Buscar";
+            buttonBusca.UseVisualStyleBackColor = true;
+            // 
+            // buttonExcluir
+            // 
+            buttonExcluir.Location = new Point(130, 186);
+            buttonExcluir.Name = "buttonExcluir";
+            buttonExcluir.Size = new Size(97, 35);
+            buttonExcluir.TabIndex = 17;
+            buttonExcluir.Text = "Excluir";
+            buttonExcluir.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
@@ -189,6 +320,8 @@
             tabControl1.ResumeLayout(false);
             tabPage1.ResumeLayout(false);
             tabPage1.PerformLayout();
+            tabPage2.ResumeLayout(false);
+            tabPage2.PerformLayout();
             ResumeLayout(false);
         }
 
@@ -208,5 +341,17 @@
         private TextBox textBoxNumeroDoDocumento;
         private TextBox textBoxNomeCompleto;
         private Button button1;
+        private Label label6;
+        private Label label7;
+        private Label label8;
+        private Label label9;
+        private Label label10;
+        private Label labelNomeCompleto;
+        private Label labelNumeroCasa;
+        private Label labelNomeRua;
+        private Label labelDataNascimento;
+        private TextBox textBox1;
+        private Button buttonExcluir;
+        private Button buttonBusca;
     }
 }
